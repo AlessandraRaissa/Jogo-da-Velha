9d292aeae95f4bd77799d9ca6585800d3b54229f
Alessandra Raissa <alessandraraissa04@gmail.com>
2023-05-06 00:31:04 -0300

    design

---
+++ src/jogodavelha/Jogodavelha.java
@@ -0,0 +1,31 @@
+package jogodavelha;
+
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.BorderFactory;
+/**
+ *
+ * @author Ica&Gabi
+ */
+public class Jogodavelha {
+    
+    boolean jogador = false;
+    
+    public void turno(){ //verifica vez do jogador
+        if(jogador){
+        }
+    }
+
+    public void vencedor(){ //verifica o vencedor
+
+    }
+
+    public void ganhaX(int x, int y, int z){ //jogadas X para vencer
+
+    }
+
+    public void ganhaO(int x, int y, int z){ //jogadas O para vencer
+
+    }
+ 
+}
